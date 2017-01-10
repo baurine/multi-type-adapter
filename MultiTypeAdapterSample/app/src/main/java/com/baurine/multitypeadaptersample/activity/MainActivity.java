0547d8ed0e7a0cf6f5da746221ccef0992292c09
baurine <2008.hbl@gmail.com>
2017-01-10 09:52:24 +0800

    do some prepare

--- MultiTypeAdapterSample/app/src/main/java/com/baurine/multitypeadaptersample/activity/MainActivity.java
+++ MultiTypeAdapterSample/app/src/main/java/com/baurine/multitypeadaptersample/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.baurine.multitypeadaptersample;
+package com.baurine.multitypeadaptersample.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.baurine.multitypeadaptersample.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ MultiTypeAdapterSample/app/src/main/java/com/baurine/multitypeadaptersample/util/BindingUtil.java
@@ -0,0 +1,21 @@
+package com.baurine.multitypeadaptersample.util;
+
+import android.databinding.BindingAdapter;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class BindingUtil {
+    @BindingAdapter({"imageUrl", "error", "placeholder"})
+    public static void loadImage(ImageView imgView,
+                                 String url,
+                                 Drawable error,
+                                 Drawable placeholder) {
+        Glide.with(imgView.getContext())
+                .load(url)
+                .error(error)
+                .placeholder(placeholder)
+                .into(imgView);
+    }
+}
