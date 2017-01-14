426dd1664eb481b29ad6d65bb8630d9eb10b8d31
baurine <2008.hbl@gmail.com>
2017-01-14 11:04:07 +0800

    prepare, init databinding

--- MultiTypeAdapterTutorial/app/src/main/java/com/baurine/multitypeadaptertutorial/activity/MainActivity.java
+++ MultiTypeAdapterTutorial/app/src/main/java/com/baurine/multitypeadaptertutorial/activity/MainActivity.java
@@ -1,13 +1,16 @@
-package com.baurine.multitypeadaptertutorial;
+package com.baurine.multitypeadaptertutorial.activity;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.baurine.multitypeadaptertutorial.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
