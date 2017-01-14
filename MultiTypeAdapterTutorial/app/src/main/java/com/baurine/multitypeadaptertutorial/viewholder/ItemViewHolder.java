a649893fac1e4db0d32d579507db7d7c8e0c5974
baurine <2008.hbl@gmail.com>
2017-01-14 21:45:09 +0800

    use ViewDataBinding.setVariable() method to unify all ViewHolder

--- MultiTypeAdapterTutorial/app/src/main/java/com/baurine/multitypeadaptertutorial/viewholder/ItemViewHolder.java
+++ MultiTypeAdapterTutorial/app/src/main/java/com/baurine/multitypeadaptertutorial/viewholder/ItemViewHolder.java
@@ -3,19 +3,22 @@ package com.baurine.multitypeadaptertutorial.viewholder;
 import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
 
+import com.android.databinding.library.baseAdapters.BR;
 import com.baurine.multitypeadaptertutorial.adapter.MulitTypeAdapter;
 
 /**
  * Created by baurine on 1/14/17.
  */
 
-public abstract class ItemViewHolder extends RecyclerView.ViewHolder {
-    protected final ViewDataBinding binding;
+public class ItemViewHolder extends RecyclerView.ViewHolder {
+    private final ViewDataBinding binding;
 
     public ItemViewHolder(ViewDataBinding binding) {
         super(binding.getRoot());
         this.binding = binding;
     }
 
-    public abstract void bindTo(MulitTypeAdapter.IItem item);
+    public void bindTo(MulitTypeAdapter.IItem item) {
+        binding.setVariable(BR.item, item);
+    }
 }
--- MultiTypeAdapterTutorial/app/src/main/java/com/baurine/multitypeadaptertutorial/viewholder/ViewHolderFactory.java
+++ MultiTypeAdapterTutorial/app/src/main/java/com/baurine/multitypeadaptertutorial/viewholder/ViewHolderFactory.java
@@ -5,8 +5,6 @@ import android.databinding.ViewDataBinding;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-import com.baurine.multitypeadaptertutorial.R;
-
 /**
  * Created by baurine on 1/14/17.
  */
@@ -15,13 +13,6 @@ public class ViewHolderFactory {
     public static ItemViewHolder create(ViewGroup parent, int viewType) {
         ViewDataBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
                 viewType, parent, false);
-        switch (viewType) {
-            case R.layout.item_image:
-                return new ImageViewHolder(binding);
-            case R.layout.item_text:
-                return new TextViewHolder(binding);
-            default:
-                return null;
-        }
+        return new ItemViewHolder(binding);
     }
 }
