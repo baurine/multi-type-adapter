4efacd03f74c76b2cd321e0949515d584018be55
baurine <2008.hbl@gmail.com>
2017-01-16 19:35:51 +0800

    user MVP to simplify the logic, abstract MainPresenter from MainActivity
    tutorial_step_16

--- MultiTypeAdapterTutorial/app/src/main/java/com/baurine/multitypeadaptertutorial/activity/MainActivity.java
+++ MultiTypeAdapterTutorial/app/src/main/java/com/baurine/multitypeadaptertutorial/activity/MainActivity.java
@@ -2,39 +2,19 @@ package com.baurine.multitypeadaptertutorial.activity;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 
-import com.baurine.multitypeadapter.MultiTypeAdapter;
 import com.baurine.multitypeadaptertutorial.R;
 import com.baurine.multitypeadaptertutorial.databinding.ActivityMainBinding;
-import com.baurine.multitypeadaptertutorial.item.EmptyItem;
-import com.baurine.multitypeadaptertutorial.item.ErrorItem;
-import com.baurine.multitypeadaptertutorial.item.FooterItem;
-import com.baurine.multitypeadaptertutorial.item.HeaderItem;
-import com.baurine.multitypeadaptertutorial.model.ModelFaker;
+import com.baurine.multitypeadaptertutorial.presenter.MainPresenter;
 
-import java.util.Random;
-
-public class MainActivity extends AppCompatActivity {
-
-    private MultiTypeAdapter adapter = new MultiTypeAdapter();
+public class MainActivity extends AppCompatActivity implements RefreshingView {
+    private MainPresenter mainPresenter;
     private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView recyclerView;
-
-    private HeaderItem headerItem;
-    private EmptyItem emptyItem;
-    private ErrorItem errorItem;
-    private FooterItem footerItem;
-
-    private boolean refreshing = false;
-    private boolean loading = false;
-    private boolean hasMoreData = true;
-    private static final int PER_PAGE_COUNT = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +22,10 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding =
                 DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        mainPresenter = new MainPresenter(this);
         swipeRefreshLayout = binding.swipeRefreshLayout;
         recyclerView = binding.recyclerView;
         initViews();
-        initItems();
     }
 
     private void initViews() {
@@ -53,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 new SwipeRefreshLayout.OnRefreshListener() {
                     @Override
                     public void onRefresh() {
-                        refreshData();
+                        mainPresenter.refreshData();
                     }
                 });
 
@@ -64,119 +44,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
                 if (newState == RecyclerView.SCROLL_STATE_IDLE &&
-                        llm.findLastVisibleItemPosition() >= adapter.getItemCount() - 1) {
-                    loadMoreData();
+                        llm.findLastVisibleItemPosition() >=
+                                mainPresenter.getAdapter().getItemCount() - 1) {
+                    mainPresenter.loadMoreData();
                 }
             }
         });
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(mainPresenter.getAdapter());
     }
 
-    private void initItems() {
-        headerItem = new HeaderItem();
-        emptyItem = new EmptyItem();
-        errorItem = new ErrorItem();
-        footerItem = new FooterItem();
-
-        emptyItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                adapter.notifyItemRemoved(adapter.removeItem(emptyItem));
-                refreshData();
-            }
-        });
-        errorItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                adapter.notifyItemRemoved(adapter.removeItem(errorItem));
-                refreshData();
-            }
-        });
-        footerItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                footerItem.setState(FooterItem.LOADING);
-                adapter.notifyItemChanged(adapter.findPos(footerItem));
-                loading = true;
-                fetchData(true);
-            }
-        });
-
-        adapter.addItem(headerItem);
-        adapter.addItem(emptyItem);
-    }
-
-    private void refreshData() {
-        if (!refreshing) {
-            refreshing = true;
-            hasMoreData = true;
-            swipeRefreshLayout.setRefreshing(true);
-            // remove all other items, just keep headerItem
-            adapter.setItem(headerItem);
-            fetchData(false);
-        }
-    }
-
-    private void loadMoreData() {
-        if (hasMoreData &&
-                !loading &&
-                // here the threshold depends on your actual situation
-                // adapter.getItemCount() > PER_PAGE_COUNT
-                adapter.getItemCount() > 2) {
-            loading = true;
-            fetchData(true);
-        }
-    }
-
-    ////////////////////////////////////////////////////////
-    // fetch data from server, for refreshing or load more
-    private void fetchData(final boolean loadMore) {
-        // mock network request
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (refreshing) {
-                    refreshing = false;
-                    swipeRefreshLayout.setRefreshing(false);
-                }
-                if (loadMore) {
-                    loading = false;
-                    adapter.removeItem(footerItem);
-                }
-                retrieveItems(loadMore);
-                adapter.notifyDataSetChanged();
-            }
-        }, 2000);
-    }
-
-    private void retrieveItems(boolean loadMore) {
-        // result = 0, network error
-        // result = 1, empty or last page data
-        // result = 2 and other, normal result
-        int resultType = (new Random()).nextInt(100) % 4;
-        if (resultType == 0) {
-            adapter.addItem(loadMore ? footerItem.setState(FooterItem.ERROR) : errorItem);
-        } else if (resultType == 1) {
-            if (loadMore) {
-                hasMoreData = false;
-                addDataItems(PER_PAGE_COUNT / 2);
-                // here depends whether you want to display no more data state
-                // if you don't want to display this state when has no more data
-                // then just don't add it back
-                adapter.addItem(footerItem.setState(FooterItem.NO_MORE));
-            } else {
-                adapter.addItem(emptyItem);
-            }
-        } else {
-            addDataItems(PER_PAGE_COUNT);
-            // pre-display loading state to improve user experience
-            adapter.addItem(footerItem.setState(FooterItem.LOADING));
-        }
-    }
-
-    private void addDataItems(int count) {
-        for (int i = 0; i < count; i++) {
-            adapter.addItem(ModelFaker.fake().createItem(adapter));
-        }
+    @Override
+    public void setRefreshing(boolean refreshing) {
+        swipeRefreshLayout.setRefreshing(refreshing);
     }
 }
